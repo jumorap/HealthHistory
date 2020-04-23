4d1093d91b888f95ff8e2e62e222d0229a3b0c52
JuanMora2017 <juan120101@hotmail.com>
2020-04-22 22:57:54 -0500

    Access details

--- app/src/main/java/com/health/AccessActivity.java
+++ app/src/main/java/com/health/AccessActivity.java
@@ -3,11 +3,19 @@ package com.health;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AccessActivity extends AppCompatActivity {
 public static final String user = "names";
 TextView txtUser;
+private ImageButton mSignout;
+private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +24,16 @@ TextView txtUser;
         txtUser = (TextView) findViewById(R.id.welcome);
         String user = getIntent().getStringExtra("names");
         txtUser.setText("¡Bienvenido " + user + "!");
+
+        mAuth = FirebaseAuth.getInstance();
+        mSignout = (ImageButton) findViewById(R.id.btnSignout);
+
+        mSignout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+            }
+        });
+
     }
 }
